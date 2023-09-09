7c69a52329dac7cb6fd8929329a6babd8b7ce778
damienbod <damien_bod@hotmail.com>
2023-09-09 07:41:29 +0200

    Azure AD B2C init angular BFF

---
+++ server/Services/MsGraphService.cs
@@ -0,0 +1,48 @@
+﻿
+using Microsoft.Graph.Users.Item.GetMemberGroups;
+using Microsoft.Graph.Models;
+
+namespace BffAzureAdB2c.Server.Services;
+
+public class MsGraphService
+{
+    private readonly GraphApplicationClientService _graphApplicationClientService;
+
+    public MsGraphService(GraphApplicationClientService graphApplicationClientService)
+    {
+        _graphApplicationClientService = graphApplicationClientService;
+    }
+
+    public async Task<User?> GetGraphApiUser(string userId)
+    {
+        var graphServiceClient = _graphApplicationClientService
+            .GetGraphClientWithClientSecretCredential();
+
+        return await graphServiceClient.Users[userId]
+            .GetAsync();
+    }
+
+    public async Task<AppRoleAssignmentCollectionResponse?> GetGraphApiUserAppRoles(string userId)
+    {
+        var graphServiceClient = _graphApplicationClientService
+            .GetGraphClientWithClientSecretCredential();
+
+        return await graphServiceClient.Users[userId]
+            .AppRoleAssignments
+            .GetAsync();
+    }
+
+    public async Task<GetMemberGroupsResponse?> GetGraphApiUserMemberGroups(string userId)
+    {
+        var graphServiceClient = _graphApplicationClientService.GetGraphClientWithClientSecretCredential();
+
+        var requestBody = new GetMemberGroupsPostRequestBody
+        {
+            SecurityEnabledOnly = true,
+        };
+
+        return await graphServiceClient.Users[userId]
+            .GetMemberGroups
+            .PostAsync(requestBody);
+    }
+}
